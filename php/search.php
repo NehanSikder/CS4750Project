89a7922c3f15760f6239a8523d597fe1ff107876
Nehan <ans2ur@virginia.edu>
2018-04-03 22:28:45 -0400

    added navbar to all of the main pages

--- php/search.php
+++ php/search.php
@@ -5,6 +5,8 @@
   <script src="../js/js/jquery-1.6.2.min.js" type="text/javascript"></script> 
 	<script src="../js/js/jquery-ui-1.8.16.custom.min.js" type="text/javascript"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="stylesheet.css">
 	<!-- jQuery library -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -30,6 +32,64 @@
     Search Restaurants
 </h1>
 <body>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Restaurant Review</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li>
+          <form class="navbar-form navbar-left" action="login.php" method="post">
+            <input type="hidden" name="userName" value="<?php echo $_POST['userName'];?>">
+            <input type="hidden" name="password" value="<?php echo $_POST['password'];?>">
+            <button class="btn btn-link" type="submit" value="Home">Home</button>
+          </form>
+      </li>
+      <li>
+        <form class="navbar-form navbar-left" action="createdReviews.php" method="post">
+          <input type="hidden" name="userName" value="<?php echo $_POST['userName'];?>">
+          <input type="hidden" name="password" value="<?php echo $_POST['password'];?>">
+          <input type="hidden" name="user_id" value="<?php echo $_POST['user_id'];?>">
+          <button class="btn btn-link" type="submit" value="Reviews Posted">Reviews Posted</button>
+        </form>
+      </li>
+      <li>
+         <form class="navbar-form navbar-left" action="createdRestaurants.php" method="post">
+          <input type="hidden" name="userName" value="<?php echo $_POST['userName'];?>">
+          <input type="hidden" name="password" value="<?php echo $_POST['password'];?>">
+          <input type="hidden" name="user_id" value="<?php echo $_POST['user_id'];?>">
+          <button class="btn btn-link" type="submit" value="Restaurants Made">Restaurants Made</button>
+        </form>
+      </li>
+      <li>
+        <form class="navbar-form navbar-left" action="search.php" method="post">
+          <input type="hidden" name="userName" value="<?php echo $_POST['userName'];?>">
+          <input type="hidden" name="password" value="<?php echo $_POST['password'];?>">
+          <button class="btn btn-link" type="submit" value="Search">Search</button>
+        </form>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li>
+        <form class="navbar-form navbar-right" action="profile.php" method="post">
+          <input type="hidden" name="userName" value="<?php echo $_POST['userName'];?>">
+          <input type="hidden" name="password" value="<?php echo $_POST['password'];?>">
+          <input type="hidden" name="user_id" value="<?php echo $_POST['user_id']?>">
+          <button class="btn btn-link" type="submit" value="Profile">Profile</button>
+        </form>
+      <li>
+      </li>
+         <form class="navbar-form navbar-right" action="logout.php" method="post">
+          <input type="hidden" name="userName" value="<?php echo $_POST['userName'];?>">
+          <input type="hidden" name="password" value="<?php echo $_POST['password'];?>">
+          <button style = 'margin-right: 5px;' class="btn btn-link" type="submit" value="Profile">Logout</button>
+        </form>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 <div class="text-center">
 <div class="input-group mb-3">
 <div class="centered">
